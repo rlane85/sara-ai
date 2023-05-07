81d56980f2ed933bc4e46883c9434396571212b1
rlane85 <ryanlane1985@gmail.com>
2023-05-07 15:25:55 +0000

    removed absolute width

--- src/ChatBox/Textfield.js
+++ src/ChatBox/Textfield.js
@@ -7,7 +7,7 @@ const Textfield = ({ textMessage, setTextMessage, sendTextMessage }) => {
   return (
     <Paper
       component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", }}
     >
       <InputBase
         onKeyPress={(e) => {
