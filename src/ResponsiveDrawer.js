fb3c015c8be7cfa7e273a242f9f5c85cb389aba7
rlane85 <44262357+rlane85@users.noreply.github.com>
2023-06-29 00:08:24 +0000

    link to login

--- src/ResponsiveDrawer.js
+++ src/ResponsiveDrawer.js
@@ -1,6 +1,9 @@
 //react imports
 import { useState } from "react";
 
+//router
+import { Link } from "react-router-dom";
+
 //components
 
 //mui components
@@ -15,6 +18,7 @@ import {
 } from "@mui/material";
 
 import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const drawerWidth = 240;
 
@@ -55,6 +59,17 @@ const ResponsiveDrawer = ({ children, drawerContent }) => {
           <Typography variant="h6" noWrap component="div">
             Sara AI
           </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={null}
+            sx={{marginLeft: "auto"}}
+          >
+            <Link to="/login">
+              <AccountCircleIcon />
+            </Link>
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Box
