33636a77379312f5eddefd3540397f11fe29affd
rlane85 <44262357+rlane85@users.noreply.github.com>
2023-07-02 17:19:50 +0000

    move to controller dir

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,5 @@
 //router
-import { Form, useActionData } from 'react-router-dom'
+import { Form, useActionData } from "react-router-dom";
 
 //material components
 import {
@@ -7,59 +7,55 @@ import {
   IconButton,
   Paper,
   Divider,
-  Typography
-} from '@mui/material'
+  Typography,
+} from "@mui/material";
 
 //material icons
-import SendIcon from '@mui/icons-material/Send'
+import SendIcon from "@mui/icons-material/Send";
 
-import { login } from '../login'
+import { login } from "./controllers/login";
 
-export async function action ({ request, params }) {
-  let formData = await request.formData()
-  let user = formData.get('user')
-  let pass = formData.get('pass')
-  const response = await login(user, pass)
-  console.log(response)
-  return response.msg
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  let user = formData.get("user");
+  let pass = formData.get("pass");
+  const response = await login(user, pass);
+  console.log(response);
+  return response.msg;
 }
 
 export const Login = () => {
-  const actionData = useActionData()
+  const actionData = useActionData();
 
   return (
-    <Form method='post' id='login'>
-      <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}>
+    <Form method="post" id="login">
+      <Paper sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}>
         <InputBase
           sx={{ ml: 1, flex: 1 }}
-          inputProps={{ 'aria-label': 'user name' }}
-          placeholder='username'
-          type='text'
-          name='user'
+          inputProps={{ "aria-label": "user name" }}
+          placeholder="username"
+          type="text"
+          name="user"
         />
         <InputBase
           sx={{ ml: 1, flex: 1 }}
-          inputProps={{ 'aria-label': 'password' }}
-          placeholder='password'
-          type='text'
-          name='pass'
+          inputProps={{ "aria-label": "password" }}
+          placeholder="password"
+          type="text"
+          name="pass"
         />
 
-        <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
-          color='primary'
-          sx={{ p: '10px' }}
-          aria-label='submit login'
-          type='submit'
+          color="primary"
+          sx={{ p: "10px" }}
+          aria-label="submit login"
+          type="submit"
         >
           <SendIcon />
         </IconButton>
       </Paper>
-      <Typography>
-        Response:
-        {" "}
-        {actionData ? actionData : 'none yet'}
-      </Typography>
+      <Typography>Response: {actionData ? actionData : "none yet"}</Typography>
     </Form>
-  )
-}
+  );
+};
