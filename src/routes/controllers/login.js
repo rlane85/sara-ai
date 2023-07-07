fb8b264ae798e4f4ad73cf4204c49da108fff943
rlane85 <ryanlane1985@gmail.com>
2023-07-07 20:37:54 +0000

    format

--- src/routes/controllers/login.js
+++ src/routes/controllers/login.js
@@ -1,30 +1,28 @@
-
-
-export async function login (user, pass) {
+export async function login(user, pass) {
   const uri = `/api/auth/signin`;
   const serverRoot = process.env.REACT_APP_SERVER_ROOT;
-  const url =serverRoot + uri;
+  const url = serverRoot + uri;
   const reqBody = {
     username: user,
-    password: pass
-  }
-  console.log(url)
+    password: pass,
+  };
+  console.log(url);
 
   return fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    credentials: 'include',
-    body: JSON.stringify(reqBody)
+    credentials: "include",
+    body: JSON.stringify(reqBody),
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      return data
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data;
     })
-    .catch(e => {
-      console.log(e)
-      return e
-    })
+    .catch((e) => {
+      console.log(e);
+      return e;
+    });
 }
