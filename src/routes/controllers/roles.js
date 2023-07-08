6d073e56aa71e72161f1dff7f24038bfef0fd707
rlane85 <ryanlane1985@gmail.com>
2023-07-08 11:09:01 +0000

    remove console log

--- src/routes/controllers/roles.js
+++ src/routes/controllers/roles.js
@@ -1,14 +1,11 @@
 export async function roles() {
   const uri = `/api/roles`;
   const url = process.env.REACT_APP_SERVER_ROOT + uri;
-  console.log(url);
   return fetch(url, { 
     credentials: "include",
-    Accept: '*/*',
    })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       return data;
     })
     .catch((e) => {
