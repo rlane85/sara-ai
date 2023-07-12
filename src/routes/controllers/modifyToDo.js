68a0c6754dcf92b999a5522dde086c085ec704ca
rlane85 <ryanlane1985@gmail.com>
2023-07-12 00:52:19 +0000

    refactor to listen for websocket messages

--- src/routes/controllers/modifyToDo.js
+++ src/routes/controllers/modifyToDo.js
@@ -1,26 +1,11 @@
-export async function modifyToDo(
-  modifyType,
-  description,
-  value
-) {
+export async function modifyToDo(modifyType, description, value) {
   const uri = `/api/todo?`;
   const serverRoot = process.env.REACT_APP_SERVER_ROOT;
-  const newValues = new URLSearchParams({description, ...value})
-  const url =
-    serverRoot +
-    uri +
-    newValues.toString();
+  const newValues = new URLSearchParams({ description, ...value });
+  const url = serverRoot + uri + newValues.toString();
 
   return fetch(url, {
     method: modifyType,
     credentials: "include",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((e) => {
-      console.log(e);
-      return e;
-    });
+  });
 }
